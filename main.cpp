1014e738e2e2d6f859da03c9cb84b21540247f5a
vanessaxramirez <130528072+vanessaxramirez@users.noreply.github.com>
2025-04-04 15:28:18 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Nathan Greenfield. All rights reserved
+
+#include <iostream>
+#include "error.h"
+#include "gevent.h"
+#include "gwindow.h"
+#include "towers.h"
+#include "peg.h"
+#include "disk.h"
+
+int main(int argc, const char* argv[])
+{
+	// Function in Tower that runs entire Towers of Hanoi program
+	towerRun();
+
+	return 0;
+}
